e31ee5a9b1ab96331f2320eb967fcf8f55a0c219
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2022-12-01 09:31:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/AutoAnura /AutoAnura
+  git clone https://github.com/TeenOfFire/4GBShortner /4GBShortner
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoAnura
+  git clone $UPSTREAM_REPO /4GBShortner
 fi
-cd /AutoAnura
+cd /4GBShortner
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
