c24c4d694b259f18fd63165ad021a3761bed2f09
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2022-12-07 19:47:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,8 +12,7 @@ def is_enabled(value, default):
 
 class script(object):
     START_TXT = """<i><b>🎃 Helo {}, I'm <a href=https://telegram.me/{}>{}</a></i></b> \n\n<i><b>🎗I Can Provide You Any Movies, Web-Series, Anime, K-Dramas, Animation, etc.,</i></b>"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-Here Is The Help For My Commands"""
+    HELP_TXT = """<b>❓How To Use Me? </b> \n\n<b>❗Just Send Me Any Movie or Series Name & I Will Send It To You Here Instantly</b>"""
     ABOUT_TXT = """<i><b><u>AutoFilter + UrlShortner Bot</u></b>
 
 🔅 Want An </i><i><b>'AutoFilter + UrlShortner Bot'</b> Like Me For Your Group &amp; Earn Money Using It?
