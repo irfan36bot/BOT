9972fd2fca811b82f1c01e66cf86d3a4df1420c2
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2022-12-16 16:26:03 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -35,6 +35,6 @@ async def verupikkals(bot, message):
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"📲<b> Broadcasting </b>♻️\n\n🔸Total Users » {total_users}\n🔹Completed » {done} / {total_users}\n🔸Success » {success}\n🔹Blocked » {blocked}\n🔸Deleted » {deleted}") 
+            await sts.edit(f"📲<b> BROADCASTING </b>♻️\n•──────────────•\n\n╭🔸Total Users » {total_users}\n├🔹Completed » {done} / {total_users}\n├🔸Success » {success}\n├🔹Blocked » {blocked}\n╰🔸Deleted » {deleted}") 
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time)) 
-    await sts.edit(f"<b>📲Broadcast Completed ✅</b>\n⌚Completed in {time_taken} Seconds\n\n🔸Total Users » {total_users}\n🔹Completed » {done} / {total_users}\n🔸Success » {success}\n🔹Blocked » {blocked}\n🔸Deleted » {deleted}")
+    await sts.edit(f"<b>📲Broadcast Completed ✅</b>\n⌚ In {time_taken} Seconds\n\n╭🔸Total Users » {total_users}\n├🔹Completed » {done} / {total_users}\n├🔸Success » {success}\n├🔹Blocked » {blocked}\n╰🔸Deleted » {deleted}")
