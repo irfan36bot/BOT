ae1ebda4663019fd9edc913fbbd0801944a60935
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2022-12-18 19:30:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -39,7 +39,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("❗Honey, It's Not For You 🚫 \n❗हनी, ये तुम्हारे लिए नहीं है 🚫", show_alert=True)
+        return await query.answer("🔆 Honey, It's Not For You❗\n🔆 हनी, ये तुम्हारे लिए नहीं है❗", show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -126,7 +126,7 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("❗Honey, It's Not For You 🚫 \n❗हनी, ये तुम्हारे लिए नहीं है 🚫", show_alert=True)
+        return await query.answer("🔆 Honey, It's Not For You❗\n🔆 हनी, ये तुम्हारे लिए नहीं है❗", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.id)
@@ -201,7 +201,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 except:
                     pass
             else:
-                await query.answer("❗Honey, It's Not For You 🚫 \n❗हनी, ये तुम्हारे लिए नहीं है 🚫", show_alert=True)
+                await query.answer("🔆 Honey, It's Not For You❗\n🔆 हनी, ये तुम्हारे लिए नहीं है❗", show_alert=True)
     elif "groupcb" in query.data:
         await query.answer()
 
