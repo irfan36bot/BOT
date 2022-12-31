872eaaeb5763df63bd832e16e1cfdd4bca0f60d4
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2022-12-31 12:26:07 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -54,59 +54,6 @@ async def showid(client, message):
             quote=True
         )
 
-@Client.on_message(filters.command(["info"]))
-async def who_is(client, message):
-    # https://github.com/SpEcHiDe/PyroGramBot/blob/master/pyrobot/plugins/admemes/whois.py#L19
-    status_message = await message.reply_text(
-        "`📮Getting User Info ♻`"
-    )
-    await status_message.edit(
-        "`📮Getting User Info ♻`"
-    )
-    from_user = None
-    from_user_id, _ = extract_user(message)
-    try:
-        from_user = await client.get_users(from_user_id)
-    except Exception as error:
-        await status_message.edit(str(error))
-        return
-    if from_user is None:
-        return await status_message.edit("no valid user_id / message specified")
-    message_out_str = ""
-    message_out_str += f"<b>👤 User ›</b> <a href='tg://user?id={from_user.id}'><b>{from_user.first_name}</b></a> \n" 
-    message_out_str += f"<b>🆔 User ID ›</b> <code>{from_user.id}</code>\n"
-        
-    chat_photo = from_user.photo
-    if chat_photo:
-        local_user_photo = await client.download_media(
-            message=chat_photo.big_file_id
-        )
-        buttons = [[
-            InlineKeyboardButton('🔐 Close', callback_data='close_data')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=local_user_photo,
-            quote=True,
-            reply_markup=reply_markup,
-            caption=message_out_str,
-            parse_mode=enums.ParseMode.HTML,
-            disable_notification=True
-        )
-        os.remove(local_user_photo)
-    else:
-        buttons = [[
-            InlineKeyboardButton('🔐 Close', callback_data='close_data')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_text(
-            text=message_out_str,
-            reply_markup=reply_markup,
-            quote=True,
-            parse_mode=enums.ParseMode.HTML,
-            disable_notification=True
-        )
-    await status_message.delete()
 
 @Client.on_message(filters.command(["imdb", 'search']))
 async def imdb_search(client, message):
