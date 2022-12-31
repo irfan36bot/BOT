fd7a1309c6eea64d51f5f1613a6a784038451fe8
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2022-12-31 12:28:11 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -26,7 +26,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('📱Support📱', url=f'https://telegram.me/{SUPPORT_CHAT}')
+        InlineKeyboardButton('📱Support📱', url=f'https://telegram.me/HeroFeedbot')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
