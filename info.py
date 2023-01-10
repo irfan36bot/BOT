2503caa76934dfd1e23fb02bf7e1a03084b84e20
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-01-10 14:09:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'telegram_files')
 
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001652564383'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '5884491121')
