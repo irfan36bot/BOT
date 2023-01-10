ee5153dc8b19f156c88d189e27b7701996f0bd51
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-01-10 09:55:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://TeenFire:TeenFire@cluster0.veo2211.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'telegram_files')
 
