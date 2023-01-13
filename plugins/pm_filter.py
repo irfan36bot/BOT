af4c935243f187b125cb80ce847832dc1ea85c26
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-01-13 09:48:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -719,7 +719,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:      
-        cap = f"<b>❗Your Search Results</b> ‛{search}’ 👇"
+        cap = f"<b>🔆 Your Search Results</b> ‛{search}’ 👇"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
