221dba45e875190ee34121975a7f5e34b2fc798b
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-01-13 16:35:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeenOfFire/HeroFlix /HeroFlix
+  git clone https://github.com/TeenOfFire/Testing /Testing
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /HeroFlix
+  git clone $UPSTREAM_REPO /Testing
 fi
-cd /HeroFlix
+cd /Testing
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
