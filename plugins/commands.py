0cee8ce7c6d3dc53ca8210f5b814f989f541c74b
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-01-18 10:11:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -31,6 +31,7 @@ async def start(client, message):
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
+            await disable_web_page_preview=True
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
             await db.add_chat(message.chat.id, message.chat.title)
         return 
