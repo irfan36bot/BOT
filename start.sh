6f4c76c98e6924f4fdee3211b713434758172d50
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-01-27 18:53:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeenOfFire/Testing /Testing
+  git clone https://github.com/TeenOfFire/Sydney /Sydney
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Testing
+  git clone $UPSTREAM_REPO /Sydney
 fi
-cd /Testing
+cd /Sydney
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
